feea52202251463e6f118c7ce524e738bf3d7d24
falling-ts <zgh.yuanshang@gmail.com>
2026-08-04 14:56:18 +0800

    fix: extension import path (js/ is two levels under web root)
    - import ../../scripts/app.js resolved to /extensions/scripts/app.js (404)
    - fix to ../../../scripts/app.js -> /scripts/app.js

--- web/js/fallingts_continue.js
+++ web/js/fallingts_continue.js
@@ -1,5 +1,5 @@
-import { app } from "../../scripts/app.js";
-import { api } from "../../scripts/api.js";
+import { app } from "../../../scripts/app.js";
+import { api } from "../../../scripts/api.js";
 
 const NODE_CLASS = "FallingTSContinue";
 const PAUSED_COLOR = "#8b6914";
